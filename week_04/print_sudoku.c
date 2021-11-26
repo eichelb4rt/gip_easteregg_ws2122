55eb23054b813e3778dbf7ad5ec1875b8493e6cf
Maurice Wenig <maurice.wenig@uni-jena.de>
2021-11-26 13:04:35 +0100

    funny sudoku

--- week_04/print_sudoku.c
+++ week_04/print_sudoku.c
@@ -7,28 +7,6 @@
 #define N BOX_SIZE * BOX_SIZE
 #define MAXCHAR 1024
 
-bool is_present_in_column(int col, int num, int** grid) {
-    for (int row = 0; row < N; row++)
-        if (grid[row][col] == num)
-            return true;
-    return false;
-}
-
-bool is_present_in_row(int row, int num, int** grid) {
-    for (int col = 0; col < N; col++)
-        if (grid[row][col] == num)
-            return true;
-    return false;
-}
-
-bool is_present_in_box(int boxStartRow, int boxStartCol, int num, int** grid) {
-    for (int row = 0; row < BOX_SIZE; row++)
-        for (int col = 0; col < BOX_SIZE; col++)
-            if (grid[row + boxStartRow][col + boxStartCol] == num)
-                return true;
-    return false;
-}
-
 void print_sudoku(int** grid) {
     for (int row = 0; row < N; row++) {
         for (int col = 0; col < N; col++) {
@@ -38,7 +16,11 @@ void print_sudoku(int** grid) {
             if (col == 0) {
                 printf(" ");
             }
-            printf("%d ", grid[row][col]);
+            if (grid[row][col] == 0) {
+                printf("  ");
+            } else {
+                printf("%d ", grid[row][col]);
+            }
         }
         if (row % BOX_SIZE == BOX_SIZE - 1 && row != N - 1) {
             printf("\n");
@@ -51,35 +33,6 @@ void print_sudoku(int** grid) {
         }
         printf("\n");
     }
-}
-
-bool find_empty_place(int* row, int* col, int** grid) {
-    for (*row = 0; *row < N; ++ * row)
-        for (*col = 0; *col < N; ++ * col)
-            if (grid[*row][*col] == 0)
-                return true;
-    return false;
-}
-
-bool is_valid(int row, int col, int num, int** grid) {
-    return !is_present_in_row(row, num, grid)
-        && !is_present_in_column(col, num, grid)
-        && !is_present_in_box(row - row % BOX_SIZE, col - col % BOX_SIZE, num, grid);
-}
-
-bool solve_sudoku(int** grid) {
-    int row, col;
-    if (!find_empty_place(&row, &col, grid))
-        return true;
-    for (int num = 1; num <= N; num++) {
-        if (is_valid(row, col, num, grid)) {
-            grid[row][col] = num;
-            if (solve_sudoku(grid))
-                return true;
-            grid[row][col] = 0;
-        }
-    }
-    return false;
 }
 
 int** init_matrix(const int rows, const int columns) {
@@ -108,7 +61,7 @@ const char* get_field(char* line, int num) {
     return NULL;
 }
 
-int** read_grid(char* filename) {
+int** read_grid(const char* filename) {
     FILE* f = fopen(filename, "r");
 
     int** grid = init_matrix(N, N);
@@ -134,9 +87,6 @@ int main(int argc, char const* argv[]) {
         exit(1);
     }
     int** grid = read_grid(argv[1]);
-    if (solve_sudoku(grid))
-        print_sudoku(grid);
-    else
-        printf("No solution exists\n");
+    print_sudoku(grid);
     return 0;
 }
